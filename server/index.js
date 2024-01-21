a7ad0f58c2651e3317fd014c14215ac5247a124f
elmurod <elmurodvkh@gmail.com>
2024-01-21 23:26:40 +0500

    6-dars: Mongoose yordamida, ma'lumotlar omboridan barcha kitoblarni olish

--- server/index.js
+++ server/index.js
@@ -35,6 +35,21 @@ app.post('/books', async (req, res) => {
     }
 });
 
+// Ma'lumotlar omboridan barcha kitoblarni olish yo'lagi
+app.get('/books', async (req, res) => {
+    try {
+        const books = await Book.find({});
+
+        return res.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({ message: error.message });
+    }
+});
+
 // Ma'lumotlar omboriga ulanish
 mongoose.connect(mongodbURL)
     .then(() => {
