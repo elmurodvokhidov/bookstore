2b81644fe19d1a32010dee0833918c13ce93cede
elmurod <elmurodvkh@gmail.com>
2024-01-21 17:33:24 +0500

    2-dars: birinchi http yo'lagini yaratish

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+// Bosh sahifa
+app.get('/', (req, res) => {
+    return res.status(234).send("Bosh sahifa");
+});
+
 app.listen(PORT, () => {
     console.log(`app is listening to port: ${PORT}`);
 });
