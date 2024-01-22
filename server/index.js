3a8c271e8d93cfad016af3acd03b060727950c12
elmurod <elmurodvkh@gmail.com>
2024-01-22 13:06:58 +0500

    11-dars: Node JS-da CORS policy

--- server/index.js
+++ server/index.js
@@ -2,11 +2,23 @@ import express from "express";
 import { PORT, mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
 
 const app = express();
 
 // Middleware for parsing request body
 app.use(express.json());
+
+// Middleware for handling CORS policy
+// Option 1: Allow all origins with default of cors(*)
+// app.use(cors());
+
+// Option 2: Allow custom origins
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"]
+}));
 
 // Bosh sahifa
 app.get('/', (req, res) => {
