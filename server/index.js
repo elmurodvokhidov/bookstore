b8a5ff71c15eec009f77340f0d172e4665564040
elmurod <elmurodvkh@gmail.com>
2024-01-23 17:28:35 +0500

    14-dars: CORS Policy 1-variant lohida qo'llaniladi

--- server/index.js
+++ server/index.js
@@ -11,14 +11,14 @@ app.use(express.json());
 
 // Middleware for handling CORS policy
 // Option 1: Allow all origins with default of cors(*)
-// app.use(cors());
+app.use(cors());
 
 // Option 2: Allow custom origins
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"]
-}));
+// app.use(cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"]
+// }));
 
 // Bosh sahifa
 app.get('/', (req, res) => {
