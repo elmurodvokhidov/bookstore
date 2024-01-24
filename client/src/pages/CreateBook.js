d79dc548dd634f92bfb89326ab52b8aa34bf797e
elmurod <elmurodvkh@gmail.com>
2024-01-24 23:44:50 +0500

    16-dars: Yangi kitob qo'shish

--- client/src/pages/CreateBook.js
+++ client/src/pages/CreateBook.js
@@ -1,8 +1,77 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import BackBtn from '../components/BackBtn';
 
 function CreateBook() {
+    const [inputValue, setInputValue] = useState({
+        title: "",
+        author: "",
+        publishYear: "",
+    });
+
+    const navigate = useNavigate();
+
+    function getInputValue(e) {
+        setInputValue({
+            ...inputValue,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    function handleSaveBook() {
+        axios.post("http://localhost:5000/books", inputValue)
+            .then(() => {
+                navigate("/");
+            })
+            .catch(error => console.log(error))
+    };
+
+
     return (
-        <div>CreateBook</div>
+        <div className='p-4'>
+            <BackBtn />
+            <h1 className="text-3xl my-4">Create Book</h1>
+            <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+                <div className="my-4">
+                    <label htmlFor="title" className='text-xl mr-4 text-gray-500'>Title</label>
+                    <input
+                        type="text"
+                        value={inputValue.title}
+                        onChange={(e) => getInputValue(e)}
+                        className='border-2 border-gray-500 px-4 w-full'
+                        name='title'
+                        id='title'
+                    />
+                </div>
+
+                <div className="my-4">
+                    <label htmlFor="author" className='text-xl mr-4 text-gray-500'>Author</label>
+                    <input
+                        type="text"
+                        value={inputValue.author}
+                        onChange={(e) => getInputValue(e)}
+                        className='border-2 border-gray-500 px-4 w-full'
+                        name='author'
+                        id='author'
+                    />
+                </div>
+
+                <div className="my-4">
+                    <label htmlFor="publishYear" className='text-xl mr-4 text-gray-500'>Publish Year</label>
+                    <input
+                        type="number"
+                        value={inputValue.publishYear}
+                        onChange={(e) => getInputValue(e)}
+                        className='border-2 border-gray-500 px-4 w-full'
+                        name='publishYear'
+                        id='publishYear'
+                    />
+                </div>
+
+                <button className='p-2 bg-sky-300 m-8' onClick={handleSaveBook}>Save</button>
+            </div>
+        </div>
     )
 }
 
